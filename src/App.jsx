0806659b295e29ca871115822858dfdfd5d75e3d
Akash Kumar Singh <itsakash18.06@gmail.com>
2024-03-30 12:15:06 +0530

    Resume section added

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Home from './Pages/Home'
 import Header from './Components/Header'
 import About from './Pages/About'
+import Resume from './Pages/Resume'
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
       <Header />
       <Home />
       <About />
+      <Resume />
       {/* <p>Hello</p> */}
     </div>
   )
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -42,7 +42,57 @@ const Home = () => {
                     </p>
                     <p className='text-[#f2f2f2] font-poppins text-[1rem] max-w-[26rem] lg:max-w-[30rem] mt-1 tracking-wide'>Highly motivated and detail-oriented
                         MERN Stack Web Developer with a strong foundation in front-end and back-end technologies.</p>
-                    <Link target='_blank' to="https://drive.google.com/file/d/1r3DyCAMkcAeIE4uch_iv_6wC15jZC0Tw/view?usp=sharing" className='p-2 mt-4 static bg-transparent text-white font-[600] tracking-wide text-[1.05rem] px-4 border-[1.8px] border-[#1BFD9C] rounded-md w-fit shadow-[0px_0px_7px_#1BFD9C_inset] hover:shadow-[0px_0px_10px_#1BFD9C] transition-all duration-700'><FontAwesomeIcon className='mr-2' icon={faArrowRight} />Download CV!</Link>
+
+                    <Link target='_blank' to="https://drive.google.com/file/d/1r3DyCAMkcAeIE4uch_iv_6wC15jZC0Tw/view?usp=sharing" className="flex items-center justify-start w-full h-20 cursor-pointer">
+                        <div
+                            className="relative inline-flex items-center justify-start py-[10px] pl-4 pr-12 overflow-hidden font-semibold text-[#00b153] transition-all duration-150 ease-in-out rounded shadow hover:pl-10 hover:pr-6 bg-gray-50 dark:bg-gray-700 dark:text-white dark:hover:text-gray-200 dark:shadow-none group"
+                        >
+                            <span
+                                className="absolute bottom-0 left-0 w-full h-1 transition-all duration-150 ease-in-out bg-[#00b153] group-hover:h-full"
+                            ></span>
+                            <span
+                                className="absolute right-0 pr-4 duration-200 ease-out group-hover:translate-x-12"
+                            >
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    viewBox="0 0 24 24"
+                                    stroke="currentColor"
+                                    fill="none"
+                                    className="w-5 h-5 text-green-400"
+                                >
+                                    <path
+                                        d="M14 5l7 7m0 0l-7 7m7-7H3"
+                                        strokeWidth="2"
+                                        strokeLinejoin="round"
+                                        strokeLinecap="round"
+                                    ></path>
+                                </svg>
+                            </span>
+                            <span
+                                className="absolute left-0 pl-2.5 -translate-x-12 group-hover:translate-x-0 ease-out duration-200"
+                            >
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    viewBox="0 0 24 24"
+                                    stroke="currentColor"
+                                    fill="none"
+                                    className="w-5 h-5 text-white"
+                                >
+                                    <path
+                                        d="M14 5l7 7m0 0l-7 7m7-7H3"
+                                        strokeWidth="2"
+                                        strokeLinejoin="round"
+                                        strokeLinecap="round"
+                                    ></path>
+                                </svg>
+                            </span>
+                            <span
+                                className="relative w-full text-left transition-colors duration-200 ease-in-out group-hover:text-white dark:group-hover:text-gray-200"
+                            >Download CV</span
+                            >
+                        </div>
+                    </Link>
+
                 </div>
                 <div className='max-w-[24rem] md:max-w-[28rem] md:h-[22rem] h-[18rem] mb-4'>
                     <lottie-player src="https://lottie.host/6c93dc0b-7087-45e5-b96d-bd630a4f792a/BJN63O2GJJ.json" background="##FFFFFF" speed="1" loop autoplay direction="1" mode="normal"></lottie-player>
